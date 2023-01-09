ec498eae189302bdc9457e578c63ed7b55d59775
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-09 15:57:31 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HeaderNav from './components/HeaderNav.vue';
+import MainContent from './components/MainContent.vue';
+import FooterContent from './components/FooterContent.vue';
+
+export default {
+  components : {
+    HeaderNav,
+    MainContent,
+    FooterContent,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HeaderNav />
+  <MainContent />
+  <FooterContent />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
 </style>
