cea3d350e90fabd0ed9fd2f44ff30b2434432ee7
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-09 16:27:27 +0100

    black content completo

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -4,8 +4,27 @@
 
 <template>
 
+    <section class="black-content">
+        <h3>
+        --> Content goes here 
+        </h3>
+    </section>
+
 </template>
 
 <style  lang="scss">
+
+    section.black-content{
+        height: 100px;
+        background-color: black;
+        color: white;
+        display: flex;
+        align-items: center;
+
+        h3{
+            width: 80%;
+            margin: 0 auto;
+        }
+    }
 
 </style>
