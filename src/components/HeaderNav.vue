aa5f88fed869bfc79745827b7d786038e92f0891
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-09 16:21:04 +0100

    header completo

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -4,10 +4,62 @@
 
 <template>
 
-    
+    <header class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li>characters</li>
+                <li class="active">comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </nav>
+    </header>
 
 </template>
 
-<style scoped>
+<style lang="scss">
+header.container {
+    width: 80%;
+    margin: 0 auto;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+}
 
+div.logo {
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 80%;
+    }
+}
+
+nav {
+    display: flex;
+    align-items: center;
+
+    ul {
+        display: flex;
+        padding: 0;
+
+        li {
+            padding: .7rem;
+            list-style-type: none;
+            text-transform: uppercase;
+            font-size: .8rem;
+            font-weight: 700;
+            color: rgb(90, 90, 90);
+        }
+    }
+}
 </style>
