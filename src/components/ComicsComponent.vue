025d93ce006585a44c6c77a5a13c08a787e6f486
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-10 18:25:41 +0100

    inserimento delle card

---
+++ src/components/ComicsComponent.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    props: [
+        'comicsThumb',
+        'comicsPrice',
+        'comicsSeries',
+        'comicsType'
+    ],
+
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div class="comic">
+        <img class="" :src="comicsThumb" :alt="comicsSeries">
+        <!-- <p>
+            {{ comicsSeries }}
+        </p> -->
+    </div>
+</template>
+
+<style lang="scss">
+div.comic {
+    width: calc((100% / 6) - .8rem);
+    height: 166px;
+    overflow: hidden;
+    margin: .4rem 0.4rem;
+
+    img {
+        width: 100%;
+    }
+}
+</style>
